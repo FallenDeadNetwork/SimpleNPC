ba23711fdb8165090e990e42077fb5126e7e9590
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2022-05-01 10:41:06 +0900

    portal

--- src/brokiem/snpc/entity/npc/BlockingDeadNPC.php
+++ src/brokiem/snpc/entity/npc/BlockingDeadNPC.php
@@ -11,13 +11,18 @@ namespace brokiem\snpc\entity\npc;
 
 use brokiem\snpc\entity\BaseNPC;
 use entity_factory\CustomEntityIds;
+use fallendead\form\game\SelectMapForm;
+use fallendead\level\map;
 use pocketmine\entity\EntitySizeInfo;
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\player\Player;
 
 class BlockingDeadNPC extends BaseNPC {
 
     public float $height = 0.0;
     public float $width = 0.0;
+
+	public int $tick_counter = 0;
 
     protected function initEntity(CompoundTag $nbt): void {
         parent::initEntity($nbt);
@@ -31,4 +36,25 @@ class BlockingDeadNPC extends BaseNPC {
     public static function getNetworkTypeId(): string {
         return CustomEntityIds::BLOCKING_DEAD()->getId();
     }
+
+	public function entityBaseTick(int $tickDiff = 1) : bool{
+		if(!$this->isAlive()||$this->isClosed()){
+			return parent::entityBaseTick($tickDiff);
+		}
+		parent::entityBaseTick($tickDiff);
+		++$this->tick_counter;
+		if($this->tick_counter >= 10){
+			foreach($this->getWorld()->getPlayers() as $player){
+				if($player->getPosition()->distance($this->location) <= 9){// 3 ** 2
+					$this->onJoin($player);
+				}
+			}
+			$this->tick_counter = 0;
+		}
+		return true;
+	}
+
+	private function onJoin(Player $player) : void{
+		map::JoinGame($player);
+	}
 }
--- src/brokiem/snpc/entity/npc/JoinPortal.php
+++ src/brokiem/snpc/entity/npc/JoinPortal.php
@@ -10,16 +10,21 @@ declare(strict_types=1);
 namespace brokiem\snpc\entity\npc;
 
 use brokiem\snpc\entity\BaseNPC;
+use conquest\object\utils\GameFactory;
 use entity_factory\CustomEntityIds;
+use fallendead\level\map;
 use pocketmine\entity\EntitySizeInfo;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\player\Player;
 use pocketmine\Server;
 
 class JoinPortal extends BaseNPC {
     public float $height = 0.0;
     public float $width = 0.0;
+
+	protected int $tick_counter = 0;
 
     protected function initEntity(CompoundTag $nbt): void {
         parent::initEntity($nbt);
@@ -37,6 +42,33 @@ class JoinPortal extends BaseNPC {
             //Server::getInstance()->dispatchCommand($source->getDamager(), "/game");
         }
     }
+
+	public function entityBaseTick(int $tickDiff = 1) : bool{
+		if(!$this->isAlive()||$this->isClosed()){
+			return parent::entityBaseTick($tickDiff);
+		}
+		parent::entityBaseTick($tickDiff);
+		++$this->tick_counter;
+		if($this->tick_counter >= 10){
+			foreach($this->getWorld()->getPlayers() as $player){
+				if($player->getPosition()->distance($this->location) <= 9){// 3 ** 2
+					$this->onJoin($player);
+				}
+			}
+			$this->tick_counter = 0;
+		}
+		return true;
+	}
+
+	private function onJoin(Player $player) : void{
+		$game = GameFactory::getGame();
+		if($game === null){
+			$player->sendMessage('§c参加可能なゲームを取得できませんでした');
+			return;
+		}
+		$game->join($player);
+		$player->sendMessage('§aゲームに参加しました');
+	}
 
     public static function getNetworkTypeId(): string {
         return CustomEntityIds::JOINPORTAL()->getId();
